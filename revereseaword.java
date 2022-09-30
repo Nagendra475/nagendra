f6f678eabc57941769010788e6a5e696ba2bbd37
Nagendra475 <112462705+Nagendra475@users.noreply.github.com>
2022-09-30 14:19:47 +0530

    Add files via upload

---
+++ revereseaword.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class KboatStringSort
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = in.nextLine();
+        str = str.toLowerCase();
+        int len = str.length();
+
+        String sortedStr = ""; 
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            for (int i = 0; i < len; i++) {
+                char strCh = str.charAt(i);
+                if (ch == strCh) {
+                    sortedStr += strCh;
+        System.out.println("Alphabetical order:");
+        System.out.println(sortedStr);
+}
+}
+}
+}
+ }       
+        
